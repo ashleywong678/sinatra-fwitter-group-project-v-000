23c676780ff8c40c5e26c0514c78b43927cf4df2
Ashley Wong <ashwong04@gmail.com>
2019-03-17 15:14:59 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -63,7 +63,6 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/:id/delete' do
-    binding.pry
     @tweet = Tweet.find(params[:id])
     if logged_in? && current_user[:id] == @tweet.user_id
       @tweet.delete
